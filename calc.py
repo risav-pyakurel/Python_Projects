f9a90fb5611e0a1239f42485737113053c065e2b
risav pyakurel <pyakurel.risav@gmail.com>
2023-08-17 13:14:33 +0545

    tictactoe

--- calc.py
+++
@@ -1,7 +0,0 @@
-from tkinter import Tk, Entry, Button, StringVar
-
-class Calculator:
-    def __init__(self,master):
-        master.title("Calculator")
-        master.geometry('357*420+0+0')
-        master.config(bg='gray')
